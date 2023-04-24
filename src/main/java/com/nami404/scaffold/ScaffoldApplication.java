b37d3ad352071ec8bd746d2f9177cc9ffb33abda
zlqRichardo <2661837382@qq.com>
2023-04-24 21:48:39 +0800

    [Description]: 首次提交

---
+++ src/main/java/com/nami404/scaffold/ScaffoldApplication.java
@@ -0,0 +1,16 @@
+package com.nami404.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author nami
+ */
+@SpringBootApplication
+public class ScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApplication.class, args);
+    }
+
+}
